dd3d2c8216b88a746f992922f8af4781abfff564
«HaykOsyan <ohayk@mail.ru>
2024-10-10 18:18:13 +0400

    Task4 DequeTasks

---
+++ DequeTasks.java
@@ -0,0 +1,37 @@
+package homework4;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DequeTasks {
+    // Реализуйте метод rotateDeque в классе DequeTasks, который принимает
+    // Deque<Integer> и число n. Метод должен повернуть очередь вправо на n
+    // позиций. Если n отрицательное, повернуть влево.
+    public static void rotateDeque(Deque<Integer> numbers, int n) {
+        if (numbers.isEmpty()) {
+            System.out.println("Deque is Empty");
+            return;
+        }
+        int size = numbers.size();
+        n = n % size;
+        for (int i = 0; i < n; i++) {
+            int last = numbers.removeLast();
+            numbers.addFirst(last);
+        }
+        System.out.println(numbers);
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> numbers = new LinkedList<>();
+        numbers.add(7);
+        numbers.add(70);
+        numbers.add(711);
+        numbers.add(74);
+        numbers.add(87);
+        numbers.add(17);
+        for (int deq : numbers) {
+            System.out.println(deq);
+        }
+        rotateDeque(numbers, 3);
+    }
+}
